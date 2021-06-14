1b26be273e231b5368cbde3da724f5f961923104
tolipenev <tolis.penev@gmail.com>
2021-06-14 19:25:37 +0200

    implemented GetRate from GitHub

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace _3shape_challenge
 {
@@ -6,7 +8,25 @@ namespace _3shape_challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var token = GetUserInput(args);
+            GetRateFromGitHub(token);
         }
+
+        static string GetUserInput(string[] input)
+        {
+            return input[0];
+        } 
+        static void GetRateFromGitHub(string token)
+        {
+            var request = HttpWebRequest.Create("https://api.github.com/rate_limit");
+            request.Headers.Add(HttpRequestHeader.Accept, "application/vnd.github.v3+json");
+            request.Headers.Add(HttpRequestHeader.UserAgent, "usertest");
+            var responce = request.GetResponse();
+            var stream = responce.GetResponseStream();
+            var reader = new StreamReader(stream);
+            var dataread = reader.ReadToEnd();
+
+        }
+        
     }
 }
